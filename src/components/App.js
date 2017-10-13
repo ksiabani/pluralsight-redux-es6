ec88ac8a345d07e3e928c3f7a2c32370868875f5
Kostas Siabanis <k.siabanis@exus.co.uk>
2017-10-13 12:33:10 +0300

    Imply to naming conventions for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // This component handles the App template used on every page
 import React, {PropTypes} from 'react';
-import Header from './common/header';
+import Header from './common/Header';
 import {connect} from 'react-redux';
 
 class App extends React.Component {
